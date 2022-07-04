28c1ac9975445a4b34a3324ac7974d1ec5eb1bc7
izzy122 <israel.oj.ak@gmail.com>
2022-07-04 01:44:50 -0700

    this is an automatic commit message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -> prints me a string using printf
+ * Return: success as 0
+ */
+int main(void)
+{
+printf(" with proper grammar, but the outcome is a piece of art");
+return (0);
+}
