7c543d95f1dbccdc96ca9bc66a1edf66743cb834
izzy122 <israel.oj.ak@gmail.com>
2022-07-06 20:05:12 -0700

    this is an automatic commit message

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
-/** 
+/**
  *print_alphabets -> prints alphabets
  *
  */
 void print_alphabet(void);
 {
-  int j;
-  for (j = 'a'; j <= 'z'; j++
-    {
-    _putchar(j);
-    }
+int j;
+for (j = 'a'; j <= 'z'; j++
+{
+_putchar(j);
+}
 _putchar('\n');
 }
