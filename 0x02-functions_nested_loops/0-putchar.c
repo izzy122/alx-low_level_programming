a138fa50e1dd9ccfa3c23da2a20f156d4767d87d
izzy122 <israel.oj.ak@gmail.com>
2022-07-06 19:12:55 -0700

    this is an automatic commit message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
  * main -> this is a program that makes use of the put char function
- * Returns: a sigle charatcer
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  _putchar('_');
-  _putchar('p');
-  _putchar('U');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('U');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-  return (0);
+return (0);
 
 
 }
